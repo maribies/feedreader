aba878c9e02c73ddd9a2f5ff0e162ad50150229f
Marissa <marissa.biesecker@gmail.com>
2018-07-24 00:22:17 +0100

    Add tests for RSS Feed test suites

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,11 +31,24 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('has a URL', function(){
+           for (let feed of allFeeds) {
+             expect(feed.url).toBeDefined();
+             expect(feed.url.length).not.toBe(0);
+           }
+         });
+
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('has defined name', function(){
+           for (let feed of allFeeds) {
+             expect(feed.name).toBeDefined();
+             expect(feed.name.lenght).not.toBe(0);
+           }
+         });
     });
 
 
